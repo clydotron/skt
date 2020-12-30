d950cad0b03112ef1e67316f4abb73bc5e9cc787
Brian Green <bgreen007@cox.net>
2020-12-30 12:59:46 -0600

    fix static files

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 type application struct {
@@ -10,22 +12,33 @@ type application struct {
 
 func main() {
 
-	// app := application{}
+	port := os.Getenv("PORT")
+	if port == "" {
+		addr := flag.String("addr", ":8080", "HTTP network address")
+		port = *addr
+	}
 
-	// addr := flag.String("addr", ":8080", "HTTP network address")
+	app := application{}
 
-	// fmt.Println("Listening on", *addr)
-	// http.ListenAndServe(*addr, app.routes())
+	fmt.Println("Listening on", port)
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.Handle("/api/ping", http.HandlerFunc(app.ping))
+	http.ListenAndServe(port, nil)
 
-	r := gin.Default()
-	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
-	r.Use(static.Serve("/", static.LocalFile("./web", true)))
-	api := r.Group("/api")
-	api.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "poopy",
-		})
-	})
+	// 	r := gin.Default()
+	// 	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
+	// 	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+	// 	api := r.Group("/api")
+	// 	api.GET("/ping", func(c *gin.Context) {
+	// 		c.JSON(200, gin.H{
+	// 			"message": "poop",
+	// 		})
+	// 	})
 
-	r.Run()
+	// 	r.Run()
 }
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
--- server/routes.go
+++ server/routes.go
@@ -10,6 +10,7 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(app.home))
+	//mux.Get("/", http.FileServer(http.Dir("./web")))
 	mux.Get("/api/ping", http.HandlerFunc(app.ping))
 	return mux
 }
