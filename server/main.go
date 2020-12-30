184a15657797ce476a679b7ba56926ceb4c90492
Brian Green <bgreen007@cox.net>
2020-12-30 08:06:45 -0600

    use gin

--- server/main.go
+++ server/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
 type application struct {
@@ -11,11 +10,22 @@ type application struct {
 
 func main() {
 
-	app := application{}
+	// app := application{}
 
-	addr := flag.String("addr", ":8080", "HTTP network address")
+	// addr := flag.String("addr", ":8080", "HTTP network address")
 
-	fmt.Println("Listening on", *addr)
-	http.ListenAndServe(*addr, app.routes())
+	// fmt.Println("Listening on", *addr)
+	// http.ListenAndServe(*addr, app.routes())
 
+	r := gin.Default()
+	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
+	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+	api := r.Group("/api")
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "poopy",
+		})
+	})
+
+	r.Run()
 }
